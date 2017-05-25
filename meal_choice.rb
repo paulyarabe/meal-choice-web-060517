aa225e1d3744158cea10e362f0a2013e2e1388dc
Paul Yarabe <pyarabe@mba2018.hbs.edu>
2017-05-25 15:46:25 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,9 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+require 'pry'
+
+def meal_choice(arg1="meat")
+  arg1
+end
+
+meal_choice()
